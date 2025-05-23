c38c76aa381f8e6d2cb4397b32427577a5d0b423
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-05-23 11:52:52 +0800

    feat: update icons widget with color attributes and add dotted_border dependency

--- lib/widget/icons.dart
+++ lib/widget/icons.dart
@@ -4,11 +4,29 @@ import 'package:flutter/material.dart';
 
 List<Icon> getIcons() {
   return const [
-    Icon(IconData(personIcon, fontFamily: 'MaterialIcons')),
-    Icon(IconData(worklcon, fontFamily: 'MaterialIcons')),
-    Icon(IconData(movieIcon, fontFamily: 'MaterialIcons')),
-    Icon(IconData(sporticon, fontFamily: 'MaterialIcons')),
-    Icon(IconData(travelIcon, fontFamily: 'MaterialIcons')),
-    Icon(IconData(shoplcon, fontFamily: 'MaterialIcons')),
+    Icon(
+      IconData(personIcon, fontFamily: 'MaterialIcons'),
+      color: purple,
+    ),
+    Icon(
+      IconData(worklcon, fontFamily: 'MaterialIcons'),
+      color: pink,
+    ),
+    Icon(
+      IconData(movieIcon, fontFamily: 'MaterialIcons'),
+      color: green,
+    ),
+    Icon(
+      IconData(sporticon, fontFamily: 'MaterialIcons'),
+      color: yellow,
+    ),
+    Icon(
+      IconData(travelIcon, fontFamily: 'MaterialIcons'),
+      color: deepPink,
+    ),
+    Icon(
+      IconData(shoplcon, fontFamily: 'MaterialIcons'),
+      color: lightBlue,
+    ),
   ];
 }
