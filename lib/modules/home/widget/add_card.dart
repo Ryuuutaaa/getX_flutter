6b3dd50d6bcc32ba7d58329cc6676e5293134bb9
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-05-23 12:02:06 +0800

    feat: implement AddCard widget for displaying interactive cards in the home module

---
+++ lib/modules/home/widget/add_card.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx_todolist/app/core/utils/extension.dart';
+import 'package:getx_todolist/modules/home/controller.dart';
+import 'package:getx_todolist/widget/icons.dart';
+
+class AddCard extends StatelessWidget {
+  final homeCtrl = Get.find<HomeController>();
+  AddCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final icons = getIcons();
+    var squareWidth = Get.width - 12.0.wp;
+    return Container(
+      width: squareWidth / 2,
+      height: squareWidth / 2,
+      margin: EdgeInsets.all(3.0.wp),
+      child: InkWell(
+        onTap: () {},
+      ),
+    );
+  }
+}
