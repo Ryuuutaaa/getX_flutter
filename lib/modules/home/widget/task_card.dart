1531d69f1829c2f7c34358918143375b786ecf47
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-05-24 19:33:08 +0800

    feat: add TaskCard widget for displaying tasks with dynamic styling

---
+++ lib/modules/home/widget/task_card.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx_todolist/app/core/utils/extension.dart';
+import 'package:getx_todolist/app/data/models/task.dart';
+import 'package:getx_todolist/modules/home/controller.dart';
+
+class TaskCard extends StatelessWidget {
+  final homeCtrl = Get.find<HomeController>();
+  final Task task;
+  TaskCard({super.key, required this.task});
+
+  @override
+  Widget build(BuildContext context) {
+    final color = HexColor.fromHex(task.color);
+    final squareWidth = Get.width - 12.0.wp;
+    return Container(
+      width: squareWidth / 2,
+      height: squareWidth / 2,
+      margin: EdgeInsets.all(3.0.wp),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [],
+      ),
+    );
+  }
+}
