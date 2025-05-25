726eee830f68b9a6d0a19a4232ab78031af945a1
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-05-26 03:51:05 +0800

    feat: integrate StepProgressIndicator for task progress visualization in TaskCard widget

--- lib/modules/home/view.dart
+++ lib/modules/home/view.dart
@@ -1,36 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:getx_todolist/app/data/models/task.dart';
 import 'package:getx_todolist/modules/home/controller.dart';
 import 'package:getx_todolist/app/core/utils/extension.dart';
 import 'package:getx_todolist/modules/home/widget/add_card.dart';
+import 'package:getx_todolist/modules/home/widget/task_card.dart';
 
 class HomePage extends GetView<HomeController> {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      child: ListView(
-        children: [
-          Padding(
-            padding: EdgeInsets.all(4.0.wp),
-            child: Text(
-              "My list",
-              style: TextStyle(
-                fontSize: 24.0.sp,
-                fontWeight: FontWeight.bold,
+      body: SafeArea(
+        child: ListView(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(4.0.wp),
+              child: Text(
+                "My list",
+                style: TextStyle(
+                  fontSize: 24.0.sp,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-          ),
-          GridView.count(
-            crossAxisCount: 2,
-            shrinkWrap: true,
-            physics: const ClampingScrollPhysics(),
-            children: [AddCard()],
-          )
-        ],
+            Obx(
+              () {
+                return GridView.count(
+                  crossAxisCount: 2,
+                  shrinkWrap: true,
+                  physics: const ClampingScrollPhysics(),
+                  children: [
+                    ...controller.tasks
+                        .map((task) => TaskCard(task: task))
+                        .toList(),
+                    AddCard(),
+                  ],
+                );
+              },
+            ),
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
--- lib/modules/home/widget/task_card.dart
+++ lib/modules/home/widget/task_card.dart
@@ -3,9 +3,10 @@ import 'package:get/get.dart';
 import 'package:getx_todolist/app/core/utils/extension.dart';
 import 'package:getx_todolist/app/data/models/task.dart';
 import 'package:getx_todolist/modules/home/controller.dart';
+import 'package:step_progress_indicator/step_progress_indicator.dart';
 
 class TaskCard extends StatelessWidget {
-  final homeCtrl = Get.find<HomeController>();
+  final HomeController homeCtrl = Get.find();
   final Task task;
   TaskCard({super.key, required this.task});
 
@@ -20,9 +21,73 @@ class TaskCard extends StatelessWidget {
       decoration: BoxDecoration(
         color: Colors.white,
         boxShadow: [
-          color : Colors.grey[300]!, 
-          blurRadius : 7, 
-          offset : const Offset(0, 7)
+          BoxShadow(
+            color: Colors.grey[300]!,
+            blurRadius: 7,
+            offset: const Offset(0, 7),
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 10.0.wp,
+            child: Row(
+              children: [
+                SizedBox(
+                  width: 25.0.wp,
+                  child: StepProgressIndicator(
+                    totalSteps: 100,
+                    currentStep: task.todos!.length,
+                    size: 8,
+                    padding: 0,
+                    selectedGradientColor: LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      colors: [color.withOpacity(0.5), color],
+                    ),
+                    unselectedGradientColor: const LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      colors: [Colors.white, Colors.white],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(6.0.wp),
+                  child: Icon(
+                    IconData(task.icon, fontFamily: 'MaterialIcons'),
+                    color: color,
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      task.title,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 12.0.wp,
+                      ),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    Text(
+                      '${task.todos!.length} Tasks',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey,
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+          Text(
+            task.title,
+            style: TextStyle(fontSize: 12.0.sp),
+          ),
         ],
       ),
     );
