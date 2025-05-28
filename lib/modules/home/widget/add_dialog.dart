1ba1dd40fd9cd4edc28f84706bc4c666da4887a4
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-05-28 18:54:04 +0800

    feat: enhance AddDialog with task selection and input validation

--- lib/modules/home/widget/add_dialog.dart
+++ lib/modules/home/widget/add_dialog.dart
@@ -44,28 +44,82 @@ class AddDialog extends StatelessWidget {
               child: Text(
                 'New Task',
                 style: TextStyle(
-                  fontSize: 20.0.wp,
+                  fontSize: 20.0.sp,
                   fontWeight: FontWeight.bold,
                 ),
               ),
             ),
-            TextFormField(
-              controller: homeCtrl.editCtrl,
-              decoration: InputDecoration(
-                focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.grey[400]!,
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 5.0.wp),
+              child: TextFormField(
+                controller: homeCtrl.editCtrl,
+                decoration: InputDecoration(
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey[400]!,
+                    ),
                   ),
                 ),
+                autofocus: true,
+                validator: (value) {
+                  if (value == null || value.trim().isEmpty) {
+                    return 'Please enter your task title';
+                  }
+                  return null;
+                },
               ),
-              autofocus: true,
-              validator: (value) {
-                if (value == null || value.trim().isEmpty) {
-                  return 'Please enter your task title';
-                }
-                return null;
-              },
             ),
+            Padding(
+              padding: EdgeInsets.only(
+                top: 5.0.wp,
+                left: 5.0.wp,
+                right: 5.0.wp,
+                bottom: 2.0.wp,
+              ),
+              child: Text(
+                'Add to',
+                style: TextStyle(
+                  fontSize: 14.0.sp,
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+            ...homeCtrl.tasks
+                .map(
+                  (element) => InkWell(
+                    onTap: () => homeCtrl.changeTask(element),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 3.0.wp,
+                        vertical: 5.0.wp,
+                      ),
+                      child: Row(
+                        children: [
+                          Icon(
+                            IconData(element.icon, fontFamily: 'MaterialIcons'),
+                            color: HexColor.fromHex(element.color),
+                          ),
+                          SizedBox(
+                            width: 3.0.wp,
+                          ),
+                          Text(
+                            element.title,
+                            style: TextStyle(
+                              fontSize: 12.0.sp,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          if (homeCtrl.task.value == element)
+                            const Icon(
+                              Icons.check,
+                              color: Colors.blue,
+                            ),
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+                .toList(),
           ],
         ),
       ),
