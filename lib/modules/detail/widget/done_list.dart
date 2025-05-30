4c6ea3bc3c39629ed06c0c467bcf0850b985a3f1
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-05-31 01:41:31 +0800

    feat: implement DoneList widget to display completed tasks

--- lib/modules/detail/widget/done_list.dart
+++ lib/modules/detail/widget/done_list.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx_todolist/modules/home/controller.dart';
 
+class DoneList extends StatelessWidget {
+  final homeCtrl = Get.find<HomeController>();
+  DoneList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(() => homeCtrl.doneTodos.isNotEmpty
+        ? ListView(
+            shrinkWrap: true,
+            physics: const ClampingScrollPhysics(),
+            children: [Text('Completed (${homeCtrl.doneTodos.length})')])
+        : Container());
+  }
+}
