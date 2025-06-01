2c06d2725593b137d70a1dc6ce30c2f8a4b8fbe9
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-06-02 00:54:27 +0800

    feat: refactor ReportPage to use Obx for reactive task data display and improve UI structure

--- lib/modules/report/view.dart
+++ lib/modules/report/view.dart
@@ -1,30 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:getx_todolist/app/data/models/task.dart';
-import 'package:getx_todolist/modules/home/controller.dart';
 import 'package:getx_todolist/app/core/utils/extension.dart';
-import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:getx_todolist/modules/home/controller.dart';
 
-class ReportPage extends GetView<HomeController> {
-  const ReportPage({super.key});
+class ReportPage extends StatelessWidget {
+  final homeCtrl = Get.find<HomeController>();
+  ReportPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Task Report'),
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () => Get.back(),
-        ),
-      ),
-      body: Center(
-        child: Text(
-          'No Report Data Available',
-          style: TextStyle(
-            fontSize: 16.0.sp,
-            fontWeight: FontWeight.w500,
-          ),
+      body: SafeArea(
+        child: Obx(
+          () {
+            var createdTasks = homeCtrl.getTotalTask();
+            var complatedTasks = homeCtrl.getTotalDoneTask();
+            var liveTasks = createdTasks - complatedTasks;
+            var precent = (complatedTasks / createdTasks) * 100;
+            return ListView(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(4.0.wp),
+                  child: Text(
+                    'My Report',
+                    style: TextStyle(
+                      fontSize: 24.0.sp,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            );
+          },
         ),
       ),
     );
