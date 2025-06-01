3721421ee6a4298c5d32ab5bd52cf10a2e8edfa1
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-06-02 01:06:26 +0800

    feat: add date display and task status summary to ReportPage

--- lib/modules/report/view.dart
+++ lib/modules/report/view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:getx_todolist/app/core/utils/extension.dart';
 import 'package:getx_todolist/modules/home/controller.dart';
+import 'package:intl/intl.dart';
 
 class ReportPage extends StatelessWidget {
   final homeCtrl = Get.find<HomeController>();
@@ -29,11 +30,105 @@ class ReportPage extends StatelessWidget {
                     ),
                   ),
                 ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 4.0.wp),
+                  child: Text(
+                    DateFormat.yMMMd().format(DateTime.now()),
+                    style: TextStyle(
+                      fontSize: 14.0.sp,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 3.0.wp,
+                    vertical: 4.0.wp,
+                  ),
+                  child: const Divider(
+                    thickness: 2,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 3.0.wp,
+                    vertical: 4.0.wp,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      _buildStatus(
+                        Colors.green,
+                        liveTasks,
+                        'Live Tasks',
+                      ),
+                      _buildStatus(
+                        Colors.blue,
+                        createdTasks,
+                        'Created Tasks',
+                      ),
+                      _buildStatus(
+                        Colors.red,
+                        complatedTasks,
+                        'Completed Tasks',
+                      ),
+                      _buildStatus(
+                        Colors.red,
+                        precent.round(),
+                        'Precent',
+                      ),
+                    ],
+                  ),
+                )
               ],
             );
           },
         ),
       ),
+    );
+  }
+
+  Row _buildStatus(Color color, int number, String text) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          height: 3.0.wp,
+          width: 3.0.wp,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            border: Border.all(
+              width: 0.5.wp,
+              color: color,
+            ),
+          ),
+        ),
+        SizedBox(
+          width: 3.0.wp,
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              '$number',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16.0.sp,
+              ),
+            ),
+            SizedBox(
+              height: 2.0.wp,
+            ),
+            Text(
+              text,
+              style: TextStyle(
+                fontSize: 12.0.sp,
+                color: Colors.grey,
+              ),
+            )
+          ],
+        )
+      ],
     );
   }
 }
